677d9f1df72929fd0fb26ccd8b7832564da6c1a7
Nginze <kuugjonathan45@gmail.com>
2023-03-14 19:00:09 +0000

    login server route

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -5,7 +5,7 @@ import { Router, useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import { WebSocketContext } from "../src/contexts/WebsocketContext";
 
-const BACKEND_URI = "https://enclosure-popularity-beads-amount.trycloudflare.com";
+const BACKEND_URI = "https://drop.up.railway.app";
 const width = 600;
 const height = 600;
 
