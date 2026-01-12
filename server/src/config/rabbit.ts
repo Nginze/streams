417b70d2e2ee43f00bfa0665c3a83be4e1cfcb37
Nginze <kuugjonathan45@gmail.com>
2026-01-12 13:46:54 +0000

    rm: rabbit mq conn

--- server/src/config/rabbit.ts
+++ server/src/config/rabbit.ts
@@ -1,58 +1,58 @@
-import amqplib, { Channel, Connection } from "amqplib";
-import "dotenv/config";
-import { logger } from "./logger";
+// import amqplib, { Channel, Connection } from "amqplib";
+// import "dotenv/config";
+// import { logger } from "./logger";
 
-const recvQueue = "sendqueue";
-const sendQueue = "recvqueue";
+// const recvQueue = "sendqueue";
+// const sendQueue = "recvqueue";
 
-const cleanup = async (channel: Channel, conn: Connection) => {
-  const sendQueue = "sendqueue";
-  const recvQueue = "recvqueue";
+// const cleanup = async (channel: Channel, conn: Connection) => {
+//   const sendQueue = "sendqueue";
+//   const recvQueue = "recvqueue";
 
-  await channel.close();
-  await conn.close();
+//   await channel.close();
+//   await conn.close();
 
-  logger.log({ level: "info", message: "cleaning up channel ..." });
+//   logger.log({ level: "info", message: "cleaning up channel ..." });
 
-  process.exit(0);
-};
+//   process.exit(0);
+// };
 
-let channel: Channel;
+// let channel: Channel;
 
-try {
-  (async () => {
-    logger.log({
-      level: "info",
-      message: `connecting to, ${
-        process.env.RABBITMQ_URL || "amqp://localhost"
-      }`,
-    });
+// try {
+//   (async () => {
+//     logger.log({
+//       level: "info",
+//       message: `connecting to, ${
+//         process.env.RABBITMQ_URL || "amqp://localhost"
+//       }`,
+//     });
 
-    let conn: Connection | null = null;
+//     let conn: Connection | null = null;
 
-    try {
-      conn = await amqplib.connect(
-        process.env.RABBITMQ_URL || "amqp://localhost"
-      );
-    } catch (err) {
-      logger.log({
-        level: "error",
-        message: "unable to connect to rabbitmq",
-      });
-      return;
-    }
+//     try {
+//       conn = await amqplib.connect(
+//         process.env.RABBITMQ_URL || "amqp://localhost"
+//       );
+//     } catch (err) {
+//       logger.log({
+//         level: "error",
+//         message: "unable to connect to rabbitmq",
+//       });
+//       return;
+//     }
 
-    logger.log({ level: "info", message: "connected to rabbitmq" });
+//     logger.log({ level: "info", message: "connected to rabbitmq" });
 
-    channel = await conn.createChannel();
+//     channel = await conn.createChannel();
 
-    await Promise.all([
-      channel.assertQueue(recvQueue),
-      channel.assertQueue(sendQueue),
-    ]);
-  })();
-} catch (err) {
-  logger.log({ level: "error", message: `${err}` });
-}
+//     await Promise.all([
+//       channel.assertQueue(recvQueue),
+//       channel.assertQueue(sendQueue),
+//     ]);
+//   })();
+// } catch (err) {
+//   logger.log({ level: "error", message: `${err}` });
+// }
 
-export { channel };
+// export { channel };
